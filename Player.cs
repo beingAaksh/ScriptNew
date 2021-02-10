e8434fb6d5bfe89f6b7b8f0b22bb473d514f9bed
beingAaksh <74947468+beingAaksh@users.noreply.github.com>
2021-02-10 09:10:28 +0530

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+
+    private Vector2 targetPos;
+    public float Yincrement;
+
+    public float speed;
+
+    public float maxHeight;
+    public float minHeight;
+
+    public int health = 3;
+    public GameObject effect;
+    public Animator camAnim;
+    public Text healthDisplay;
+
+    private void Update()
+    {
+        healthDisplay.text = health.ToString();
+
+        if(health <=0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        
+        if(Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight )
+        {
+            camAnim.SetTrigger("shake");
+            Instantiate(effect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            camAnim.SetTrigger("shake");
+            Instantiate(effect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+        }
+    }
+}
